1a5737ad4a30d974c4301610d2ec23215df6ccbc
Shaine Tsou <shaineagro06@gmail.com>
2022-06-14 23:49:23 +0800

    Update description of 'Merge Strings Alternately' question

--- string/merge-strings-alternately.js
+++ string/merge-strings-alternately.js
@@ -1,7 +1,9 @@
 /* 
 Merge Strings Alternately (https://leetcode.com/problems/merge-strings-alternately/)
-- 
-
+- Given two strings word1 and word2.
+- Merge the strings by adding letters in alternating order, starting with word1.
+- If a string is longer than the other, append the additional letters into the end of the merged string
+- Return the merged string.
 */
 
 // ---- Test Cases ----
